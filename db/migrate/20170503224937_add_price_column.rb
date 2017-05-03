104d2088c4f93794fd8aaf2ac9a8eb5ece316e84
Fariba <massah.2@gmail.com>
2017-05-03 16:35:25 -0700

    Add price column to products table

---
+++ db/migrate/20170503224937_add_price_column.rb
@@ -0,0 +1,5 @@
+class AddPriceColumn < ActiveRecord::Migration
+  def change
+    add_column :products, :price_in_cents, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170219010630) do
+ActiveRecord::Schema.define(version: 20170503224937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,11 +54,12 @@ ActiveRecord::Schema.define(version: 20170219010630) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.string   "name",                         null: false
-    t.text     "description",                  null: false
-    t.boolean  "out_of_stock", default: false, null: false
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.string   "name",                           null: false
+    t.text     "description",                    null: false
+    t.boolean  "out_of_stock",   default: false, null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.integer  "price_in_cents"
   end
 
 end
